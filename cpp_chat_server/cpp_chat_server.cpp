278945230d4eb509a5396c5bc0fe98c8b490e5bd
Sylwedn <sylwedn@gmail.com>
2024-01-21 09:45:05 +0200

    Thread / chat / 100 clients

--- cpp_chat_server/cpp_chat_server.cpp
+++ cpp_chat_server/cpp_chat_server.cpp
@@ -4,6 +4,23 @@
 
 #pragma warning(disable: 4996)
 
+SOCKET Connections[100]; //massive for 100 clients
+int Counter = 0; // 0 for first user that connected
+
+void ClientHandler(int index) {
+	char msg[256];
+	while (true) {
+		recv(Connections[index], msg, sizeof(msg), NULL);
+		for (int i = 0; i < Counter; i++) {
+			if (i == index) { //avoid double send messege 
+				continue;
+			}
+
+			send(Connections[i], msg, sizeof(msg), NULL);
+		}
+	}
+}
+
 int main(int arg, char* argv[]) {  //check if lib loaded 
 	//WSASTARTUP
 	WSADATA wsaData;
@@ -15,7 +32,7 @@ int main(int arg, char* argv[]) {  //check if lib loaded
 
 
 	SOCKADDR_IN addr; //sockaddr is for saving address // name addr
-	int sizeofaddr = sizeof(addr);
+	int sizeofaddr = sizeof(addr); // sizeoff operator that counts size or datatype in bites
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //sin_addr is structure of sockaddr_in
 	addr.sin_port = htons(1111); //port 
 	addr.sin_family = AF_INET; //internet protocol AF_INET
@@ -25,14 +42,21 @@ int main(int arg, char* argv[]) {  //check if lib loaded
 	listen(sListen, SOMAXCONN);
 
 	SOCKET newConnection;
-	newConnection = accept(sListen, (sockaddr*)&addr, &sizeofaddr);
+	for (int i = 0; i < 100; i++) {
+		newConnection = accept(sListen, (sockaddr*)&addr, &sizeofaddr);
 
-	if (newConnection == 0) {
-		std::cout << "Error #2\n";
-	}else {
-		std::cout << "Client Connected!\n";
-		char msg[256] = "Hello. It's my first network program!";
-		send(newConnection, msg, sizeof(msg), NULL);
+		if (newConnection == 0) {
+			std::cout << "Error #2\n";
+		}
+		else {
+			std::cout << "Client Connected!\n";
+			char msg[256] = "Hello. It's my first network program!";
+			send(newConnection, msg, sizeof(msg), NULL); 
+
+			Connections[i] = newConnection;
+			Counter++;
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(i), NULL, NULL); // multithreading
+		}
 	}
 
 
