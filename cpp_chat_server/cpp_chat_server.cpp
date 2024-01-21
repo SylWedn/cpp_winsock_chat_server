88cadf7d73146b78006310c7643060cca4d5e9dc
Sylwedn <sylwedn@gmail.com>
2024-01-21 14:44:03 +0200

    test packet

--- cpp_chat_server/cpp_chat_server.cpp
+++ cpp_chat_server/cpp_chat_server.cpp
@@ -8,9 +8,16 @@
 SOCKET Connections[100]; //massive for 100 clients
 int Counter = 0; // 0 for first user that connected
 
-void ClientHandler(int index) {
-	int msg_size;
-	while (true) {
+enum Packet {
+	P_ChatMessage, // type of packet
+	P_Test
+};
+
+bool ProcessPacket(int index, Packet packettype) {
+	switch (packettype) {
+	case P_ChatMessage:
+	{
+		int msg_size;
 		recv(Connections[index], (char*)&msg_size, sizeof(int), NULL);
 		char* msg = new char[msg_size + 1];
 		msg[msg_size] = '\0';
@@ -19,11 +26,31 @@ void ClientHandler(int index) {
 			if (i == index) { //avoid double send messege 
 				continue;
 			}
+			Packet msgtype = P_ChatMessage;
+			send(Connections[i], (char*)&msgtype, sizeof(Packet), NULL);
 			send(Connections[i], (char*)&msg_size, sizeof(int), NULL);
 			send(Connections[i], msg, msg_size, NULL);
 		}
 
 	}
+	default:
+		std::cout << "Unrecognized packet : " << packettype << std::endl;
+		break;
+	}
+	
+	return true;
+}
+
+void ClientHandler(int index) {
+	Packet packettype;
+	while (true) {
+		recv(Connections[index], (char*)&packettype, sizeof(Packet), NULL);
+
+		if (ProcessPacket(index, packettype)) {
+			break;
+		}
+	}
+	closesocket(Connections[index]);
 }
 
 int main(int arg, char* argv[]) {  //check if lib loaded 
@@ -57,12 +84,18 @@ int main(int arg, char* argv[]) {  //check if lib loaded
 			std::cout << "Client Connected!\n";
 			std::string msg = "Hello. It's my first network program!";
 			int msg_size = msg.size();
+			Packet msgtype = P_ChatMessage;
+			send(newConnection, (char*)&msgtype, sizeof(Packet), NULL);
 			send(newConnection, (char*)&msg_size, sizeof(int), NULL);
 			send(newConnection, msg.c_str(), msg_size, NULL);
 
 			Connections[i] = newConnection;
 			Counter++;
 			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(i), NULL, NULL); // multithreading
+
+			Packet testpacket = P_Test;
+		
+			send(newConnection, (char*)&testpacket, sizeof(Packet), NULL);
 		}
 	}
 
